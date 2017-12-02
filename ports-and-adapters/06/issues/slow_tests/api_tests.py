0096e99d80788c338dd982ee408ad2dfd5fd223b
Harry <hjwp2@cantab.net>
2017-12-02 17:36:24 +0000

    api tests now use flask test client

--- ports-and-adapters/06/issues/slow_tests/api_tests.py
+++ ports-and-adapters/06/issues/slow_tests/api_tests.py
@@ -1,25 +1,31 @@
-import requests
+import json
 from expects import expect,equal
+from issues.adapters.flask import app
 
-
-def report_issue(reporter_name='fred', reporter_email='fred@example.org', problem_description='Halp!!!1!!1!11eleven!'):
+def report_issue(client, reporter_name='fred', reporter_email='fred@example.org', problem_description='Halp!!!1!!1!11eleven!'):
     data = {
             'reporter_name': reporter_name,
             'reporter_email': reporter_email,
             'problem_description': problem_description
         }
 
-    resp = requests.post('http://localhost:5000/issues', json=data)
+    resp = client.post('/issues', data=json.dumps(data), content_type='application/json')
     return resp.headers['Location']
+
 
 class When_reporting_a_new_issue:
 
+    def given_a_running_flask_app(self):
+        app.testing = True
+        self.client = app.test_client()
+
     def because_we_report_a_new_issue(self):
-        self.location = report_issue('Arnold', 'arnold@example.org', "I'm all alone and frightened.")
+        self.location = report_issue(self.client, 'Arnold', 'arnold@example.org', "I'm all alone and frightened.")
 
     @property
     def the_issue(self):
-        return requests.get(self.location).json()
+        response = self.client.get(self.location)
+        return json.loads(response.data)
 
     def it_should_have_the_correct_reporter(self):
         expect(self.the_issue['reporter_name']).to(equal('Arnold'))
@@ -30,11 +36,13 @@ class When_reporting_a_new_issue:
 
 class When_assigning_an_issue_to_another_engineer:
 
-    def given_an_issue(self):
-        self.location = report_issue('Arnold', 'arnold@example.org', "I'm all alone and frightened")
+    def given_a_running_flask_app_and_an_issue(self):
+        app.testing = True
+        self.client = app.test_client()
+        self.location = report_issue(self.client, 'Arnold', 'arnold@example.org', "I'm all alone and frightened")
 
     def because_barbara_assigns_the_issue_to_constance(self):
-        self.response = requests.post(self.location + '/assign?engineer=constance',
+        self.response = self.client.post(self.location + '/assign?engineer=constance',
                                     headers={ 'X-Email': 'barbara@example.org'})
 
     def it_should_be_fine(self):
