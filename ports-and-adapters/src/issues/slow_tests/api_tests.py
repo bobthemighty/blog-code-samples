f1fca4ec32d01bea40648f7936823f368035c35e
Harry <hjwp2@cantab.net>
2018-10-05 18:36:33 +0100

    add code to self-spawn an api server for slow tests

--- ports-and-adapters/src/issues/slow_tests/api_tests.py
+++ ports-and-adapters/src/issues/slow_tests/api_tests.py
@@ -1,5 +1,38 @@
+import time
+import sys
+from pathlib import Path
 import requests
+import subprocess
 from expects import expect, equal
+
+class GivenAnAPIServer:
+    def given_an_api_server(self):
+        cwd = Path(__file__).parent / '../..'
+        self.server = subprocess.Popen([
+            sys.executable, '-m', 'issues.adapters.flask'
+        ], cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self._wait_for_server_to_start()
+
+    def _wait_for_server_to_start(self):
+        start = time.time()
+        while time.time() < start + 3:
+            try:
+                requests.get('http://localhost:5000')
+                return
+            except Exception as e:
+                print(e, type(e))
+                time.sleep(0.2)
+        stdout, stderr = '', ''
+        rc = self.server.poll()
+        if rc:
+            stdout, stderr = self.server.stdout.read(), self.server.stderr.read()
+        raise Exception(
+            f'server did not start, return code was {rc}.\n'
+            f'stdout:\n{stdout}\n\nstderr:\n{stderr}\n'
+        )
+
+    def cleanup_server(self):
+        self.server.kill()
 
 
 def report_issue(reporter_name='fred',
@@ -15,7 +48,7 @@ def report_issue(reporter_name='fred',
     return resp.headers['Location']
 
 
-class When_reporting_a_new_issue:
+class When_reporting_a_new_issue(GivenAnAPIServer):
 
     def because_we_report_a_new_issue(self):
         self.location = report_issue('Arnold', 'arnold@example.org',
@@ -33,7 +66,7 @@ class When_reporting_a_new_issue:
             equal('I\'m all alone and frightened.'))
 
 
-class When_assigning_an_issue_to_another_engineer:
+class When_assigning_an_issue_to_another_engineer(GivenAnAPIServer):
 
     def given_an_issue(self):
         self.location = report_issue('Arnold', 'arnold@example.org',
