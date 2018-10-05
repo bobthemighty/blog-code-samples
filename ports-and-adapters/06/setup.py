7c52712481c8a5d65b4b44f6e66384e1af29051d
Harry <hjwp2@cantab.net>
2018-10-05 17:16:46 +0100

    move to a single src folder for the purposes of history

--- ports-and-adapters/06/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='ports-and-adapters',
-    url='https://github.com/bobthemighty/ports-and-adapters-pythong',
-    packages=find_packages('.'),
-)
