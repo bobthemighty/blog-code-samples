812a943435c806b160a52c47f754709ff9b2b133
Harry <hjwp2@cantab.net>
2018-10-06 08:45:49 +0100

    remove bug in stdout-emailser, stray self

--- ports-and-adapters/src/issues/adapters/emails.py
+++ ports-and-adapters/src/issues/adapters/emails.py
@@ -1,4 +1,4 @@
-def send_to_stdout(self, recipient, sender, subject, body):
+def send_to_stdout(recipient, sender, subject, body):
     print(
         "Sending email to {to} from {sender}\nsubject:{subject}\n{body}".format(
             to=recipient, sender=sender, body=body, subject=subject))
